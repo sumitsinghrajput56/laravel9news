d64e01a591135d07327de30d1f0959f2a7d0839d
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-04-21 14:28:48 +0530

    routes 14

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/contact', function () {
+    echo "this is contact page";
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
